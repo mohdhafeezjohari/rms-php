2ede75e7a09e565294f1f771393b814d6222897e
hafeez <hafeez.johari@razer.com>
2023-04-03 15:10:36 +0800

    fixed composer file

--- autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once 'vendor/autoload.php';
-
-// Register the MyLibrary namespace
-$loader = new \Composer\Autoload\ClassLoader();
-$loader->addPsr4('ApiClient\\', __DIR__ . '/src/');
-$loader->register();
